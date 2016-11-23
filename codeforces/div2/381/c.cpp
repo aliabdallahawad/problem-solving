e4d2c45e6b9e7237bd4a542e57d96129431470ae
ali-abdalla <aliabdallah93@gmail.com>
2016-11-23 21:22:30 +0200

    [codeforces] div2 - round 381 (problem c)

---
+++ codeforces/div2/381/c.cpp
@@ -0,0 +1,35 @@
+// @author aliabdallah
+// #includes {{{
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <functional>
+#include <math.h>
+#include <assert.h>
+#include <stdarg.h>
+#include <time.h>
+#include <limits.h>
+#include <ctype.h>
+#include <map>
+#include <set>
+#include <queue>
+#include <algorithm>
+#include <vector>
+#include <iostream>
+#include <sstream>
+#include <utility>
+#include <numeric>
+#include <list>
+#include <iomanip>
+#include <cmath>
+#include <stack>
+#include <complex>
+#include <valarray>
+using namespace std;
+// }}}
+
+int main()
+{
+  
+  return 0;
+}
